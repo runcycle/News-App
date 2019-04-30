09d2fe5fa41a4690bf22bd0bb891b2f865d5460b
runcycle <justinbartholomew@gmail.com>
2019-04-30 16:46:02 -0400

    Added stories_blocs.dart and stories_provider.dart

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import '../blocs/stories_provider.dart';
 
 class NewsList extends StatelessWidget {
   Widget build(context) {
+    final bloc = StoriesProvider.of(context);
+    
     return Scaffold(
       appBar: AppBar(
         title: Text('Top News'),
       ),
-      body: Text('Show some news here.'),
+      body: Text('Show a list'),
     );
   }
 }
