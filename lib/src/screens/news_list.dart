12f5cab1422f119aba3c3f7bf016dc57b0c109b3
runcycle <justinbartholomew@gmail.com>
2019-05-01 16:50:20 -0400

    Fetches story Ids and renders them to front page

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -4,12 +4,34 @@ import '../blocs/stories_provider.dart';
 class NewsList extends StatelessWidget {
   Widget build(context) {
     final bloc = StoriesProvider.of(context);
+
+    bloc.fetchTopIds();
     
     return Scaffold(
       appBar: AppBar(
         title: Text('Top News'),
       ),
-      body: Text('Show a list'),
+      body: buildList(bloc),
+    );
+  }
+
+  Widget buildList(StoriesBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.topIds,
+      builder: (context, AsyncSnapshot<List<int>> snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        return ListView.builder(
+          itemCount: snapshot.data.length,
+          itemBuilder: (context, int index) {
+            return Text('${snapshot.data[index]}');
+          },
+        );
+      },
     );
   }
 }
