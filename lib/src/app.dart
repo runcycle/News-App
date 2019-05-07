7fbe0159a1ca528a621a3c6ab14b162d3c39c87c
runcycle <justinbartholomew@gmail.com>
2019-05-07 16:52:20 -0400

    Added comment data fetching

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,20 +1,41 @@
 import 'package:flutter/material.dart';
 import 'screens/news_list.dart';
 import 'blocs/stories_provider.dart';
+import 'screens/news_detail.dart';
+import 'blocs/comments_provider.dart';
 
 class App extends StatelessWidget {
   Widget build(context) {
-    return StoriesProvider(
-      child: MaterialApp(
-        title: 'News',
-        onGenerateRoute: (RouteSettings settings) {
-          return MaterialPageRoute(
-            builder: (context) {
-              return NewsList();
-            },
-          );
-        },
+    return CommentsProvider(
+      child: StoriesProvider(
+        child: MaterialApp(
+          title: 'News',
+          onGenerateRoute: routes,
+        ),
       ),
     );
+  }
+
+  Route routes(RouteSettings settings) {
+     if (settings.name == '/') {
+       return MaterialPageRoute(
+        builder: (context) {
+          return NewsList();
+        },
+      );
+     } else {
+       return MaterialPageRoute(
+         builder: (context) {
+           final commentsBloc = CommentsProvider.of(context);
+           final itemId = int.parse(settings.name.replaceFirst('/', ''));
+
+           commentsBloc.fetchItemWithComments(itemId);
+
+           return NewsDetail(
+             itemId: itemId,
+           );
+         },
+       );
+     }
   }
 }
---
+++ lib/src/screens/news_detail.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import '../blocs/comments_provider.dart';
+import '../models/item_model.dart';
+
+class NewsDetail extends StatelessWidget {
+  final int itemId;
+
+  NewsDetail({this.itemId});
+  
+  Widget build(context) {
+    final bloc = CommentsProvider.of(context);
+    
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Detail'),
+      ),
+      body: buildBody(bloc),
+    );
+  }
+
+  Widget buildBody(CommentsBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.itemWithComments,
+      builder: (context, AsyncSnapshot<Map<int, Future<ItemModel>>> snapshot) {
+
+      }
+    );
+  }
+}
